3b9211ac6e092a5578201fd1a755d8adfd5c9adf
softsmyth <geekgineering@gmail.com>
2014-01-11 12:13:21 -0600

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'pkHWr8uC6Ra2NNgeoDR4fAnwX2Sx4Y6MczhVEc7CUUlXu7IG', // Unique master Xively API key to be used as a default
+		defaultFeeds	= ['90133,44411'], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Geekgineering Feeds', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
